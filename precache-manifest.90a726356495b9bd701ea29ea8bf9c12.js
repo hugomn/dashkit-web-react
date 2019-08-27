fdb83f4ee15a7520dc3408fc99629e63a29b92bb
Hugo <hugomn@gmail.com>
2019-08-27 16:59:06 +0000

    Automated deployment to GitHub Pages: af093c57a045107744caf2db2b91bc96a1f990c2

---
+++ precache-manifest.90a726356495b9bd701ea29ea8bf9c12.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b92819ae9cab76e058d96ea72af2aab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95346b019f6fc42d6560",
+    "url": "/static/css/2.b1119605.chunk.css"
+  },
+  {
+    "revision": "95346b019f6fc42d6560",
+    "url": "/static/js/2.6218ee0b.chunk.js"
+  },
+  {
+    "revision": "73a0e627768181d2690d",
+    "url": "/static/js/main.a340c955.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d7f50953bd8f30466233ebd47e67be9e",
+    "url": "/static/media/alert.d7f50953.svg"
+  },
+  {
+    "revision": "c63ce33a3fbb0a0b8dde6e62428febdf",
+    "url": "/static/media/check_filled.c63ce33a.svg"
+  },
+  {
+    "revision": "e66f9e00227abe513ea79c6ae5d75902",
+    "url": "/static/media/check_unfilled.e66f9e00.svg"
+  },
+  {
+    "revision": "54a481cea132e1e39fb96f02b63b698f",
+    "url": "/static/media/empty-backlog.54a481ce.svg"
+  },
+  {
+    "revision": "c3d845c0651602348ca0bd0e23d32b01",
+    "url": "/static/media/logo-full.c3d845c0.svg"
+  },
+  {
+    "revision": "a3887d566515b21220b1ddc42ba6e8c2",
+    "url": "/static/media/signout.a3887d56.svg"
+  },
+  {
+    "revision": "a221aada02ba7e0fa49b75ad78ab30e9",
+    "url": "/static/media/sort-down.a221aada.svg"
+  }
+]);
